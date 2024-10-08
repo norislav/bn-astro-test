6af58ff30cd1903c83130ff9f304a23e32bd99f6
norislav <rino.sincic0@gmail.com>
2024-10-08 14:16:08 +0200

    Update astro.config.mjs

--- astro-in-15/astro.config.mjs
+++ astro-in-15/astro.config.mjs
@@ -6,6 +6,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://norislav.github.io",
+  base: "bn-astro-test",
   integrations: [
     tailwind(),
     icon({
